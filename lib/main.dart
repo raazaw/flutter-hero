6fb9041485b6ecb2e89e3c6e6b8047f825b90185
Raj Chaulagain <chaulagainrajman@gmail.com>
2021-07-07 22:37:45 +0545

    successfully app completed

--- lib/main.dart
+++ lib/main.dart
@@ -13,52 +13,85 @@ class LangurBurja extends StatefulWidget {
   _LangurBurjaState createState() => _LangurBurjaState();
 }
 
+enum dice { CLUB, KING, HEART, FLAG, DIAMOND, SPADE }
+
 class _LangurBurjaState extends State<LangurBurja> {
-  String randomG() {
-    Random random = Random();
-    int number = random.nextInt(6) + 1;
-
-    switch (number) {
-      case 1:
-        {
-          return "clubs";
-        }
-
-      case 2:
-        {
-          return "diamond";
-        }
-
-        
-      case 3:
-        {
-          return "flag";
-        }
-
-        
-      case 4:
-        {
-          return "heart";
-        }
-
-       
-      case 5:
-        {
-          return "king";
-        }
-
-        
-      case 6:
-        {
-          return "spade";
-        }
-
-       
-      default:
-        {
-          return "-1";
-        }
+  dice? selectedDice;
+  List<dice> results = [];
+  void generateResult() {
+    results = [];
+    for (int i = 0; i < 6; i++) {
+      Random random = Random();
+      int getValue = random.nextInt(6) + 1;
+      dice result = randomDice(getValue);
+      results.add(result);
     }
+  }
+
+  dice randomDice(int value) {
+    switch (value) {
+      case 1:
+        return dice.CLUB;
+        break;
+      case 2:
+        return dice.KING;
+        break;
+      case 3:
+        return dice.HEART;
+        break;
+      case 4:
+        return dice.FLAG;
+        break;
+      case 5:
+        return dice.DIAMOND;
+        break;
+      case 6:
+        return dice.SPADE;
+        break;
+
+      default:
+        return dice.CLUB;
+    }
+  }
+
+  List<Widget> getWidgets() {
+    List<Widget> displayResults = [];
+    for (int i = 0; i < results.length; i++) {
+      displayResults.add(Expanded(
+        child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 5),
+            child: Image.asset(assetFactory(results[i]))),
+      ));
+    }
+    return displayResults;
+  }
+
+  String assetFactory(dice nice) {
+    switch (nice) {
+      case dice.CLUB:
+        return "assets/images/clubs.png";
+      case dice.KING:
+        return "assets/images/kings.png";
+      case dice.HEART:
+        return "assets/images/heart.png";
+      case dice.FLAG:
+        return "assets/images/flag.png";
+      case dice.DIAMOND:
+        return "assets/images/diamond.png";
+      case dice.SPADE:
+        return "assets/images/spade.png";
+      default:
+        return "assets/images/clubs.png";
+    }
+  }
+
+  String checkWin() {
+    for (int i = 0; i < results.length; i++) {
+      if (selectedDice == results[i]) {
+        return "Maara!!!";
+      }
+    }
+    return "Thuiyaa!!";
   }
 
   @override
@@ -68,6 +101,11 @@ class _LangurBurjaState extends State<LangurBurja> {
         appBar: AppBar(
           title: Text("Langur Burja Game"),
           centerTitle: true,
+        ),
+        drawer: Drawer(
+          elevation: 40,
+          child: Center(
+              child: Text("Namastey Raj!", style: TextStyle(fontSize: 32))),
         ),
         body: SafeArea(
           child: Column(
@@ -81,40 +119,50 @@ class _LangurBurjaState extends State<LangurBurja> {
                 children: [
                   ElevatedButton(
                     onPressed: () {
-                      setState(() {});
+                      setState(() {
+                        selectedDice = dice.CLUB;
+                      });
                     },
                     style: ElevatedButton.styleFrom(
                       elevation: 10,
-                      primary: Colors.white,
+                      primary: selectedDice == dice.CLUB
+                          ? Colors.yellow[200]
+                          : Colors.white,
                     ),
-                    child: Container(
-                      height: 100,
-                      width: 100,
+                    child: Expanded(
                       child: Image.asset("assets/images/clubs.png"),
                     ),
                   ),
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        selectedDice = dice.KING;
+                      });
+                    },
                     style: ElevatedButton.styleFrom(
                       elevation: 10,
-                      primary: Colors.white,
+                      primary: selectedDice == dice.KING
+                          ? Colors.yellow[200]
+                          : Colors.white,
                     ),
-                    child: Container(
-                      height: 100,
-                      width: 100,
-                      child: Image.asset("assets/images/clubs.png"),
+                    child: Expanded(
+                      child: Image.asset("assets/images/kings.png"),
                     ),
                   ),
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        selectedDice = dice.HEART;
+                      });
+                    },
                     style: ElevatedButton.styleFrom(
                       elevation: 10,
-                      primary: Colors.white,
+                      primary: selectedDice == dice.HEART
+                          ? Colors.yellow[200]
+                          : Colors.white,
                     ),
-                    child: Container(
-                      height: 100,
-                      width: 100,
-                      child: Image.asset("assets/images/clubs.png"),
+                    child: Expanded(
+                      child: Image.asset("assets/images/heart.png"),
                     ),
                   ),
                 ],
@@ -127,39 +175,51 @@ class _LangurBurjaState extends State<LangurBurja> {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        selectedDice = dice.FLAG;
+                      });
+                    },
                     style: ElevatedButton.styleFrom(
                       elevation: 10,
-                      primary: Colors.white,
+                      primary: selectedDice == dice.FLAG
+                          ? Colors.yellow[200]
+                          : Colors.white,
                     ),
-                    child: Container(
-                      height: 100,
-                      width: 100,
-                      child: Image.asset("assets/images/clubs.png"),
+                    child: Expanded(
+                      child: Image.asset("assets/images/flag.png"),
                     ),
                   ),
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        selectedDice = dice.DIAMOND;
+                      });
+                    },
                     style: ElevatedButton.styleFrom(
                       elevation: 10,
-                      primary: Colors.white,
+                      primary: selectedDice == dice.DIAMOND
+                          ? Colors.yellow[200]
+                          : Colors.white,
                     ),
-                    child: Container(
-                      height: 100,
-                      width: 100,
-                      child: Image.asset("assets/images/clubs.png"),
+                    child: Expanded(
+                      child: Image.asset("assets/images/diamond.png"),
                     ),
                   ),
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        selectedDice = dice.SPADE;
+                      });
+                    },
                     style: ElevatedButton.styleFrom(
                       elevation: 10,
-                      primary: Colors.white,
+                      primary: selectedDice == dice.SPADE
+                          ? Colors.yellow[200]
+                          : Colors.white,
                     ),
-                    child: Container(
-                      height: 100,
-                      width: 100,
-                      child: Image.asset("assets/images/clubs.png"),
+                    child: Expanded(
+                      child: Image.asset("assets/images/spade.png"),
                     ),
                   ),
                 ],
@@ -168,20 +228,37 @@ class _LangurBurjaState extends State<LangurBurja> {
                 height: 40,
                 width: double.infinity,
               ),
-              Container(
-                height: 60,
-                width: 200,
-                decoration: BoxDecoration(
-                  color: Colors.teal,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Center(
-                  child: Text(
-                    "Roll Dice!!",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold),
+              Visibility(
+                visible: selectedDice == null ? false : true,
+                child: Container(
+                  width: 200,
+                  height: 65,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        generateResult();
+                      });
+                    },
+                    style: ButtonStyle(
+                      // MaterialStateProperty<double?>? elevation,
+                      elevation: MaterialStateProperty.all<double>(20),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.teal),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Roll Dice!!",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
                   ),
                 ),
               ),
@@ -190,114 +267,71 @@ class _LangurBurjaState extends State<LangurBurja> {
                 width: double.infinity,
               ),
               Row(
-                children: [
-                  
-                  Expanded(child: Image.asset("assets/images/clubs.png")),
-                  Expanded(child: Image.asset("assets/images/clubs.png")),
-                  Expanded(child: Image.asset("assets/images/clubs.png")),
-                  Expanded(child: Image.asset("assets/images/clubs.png")),
-                  Expanded(child: Image.asset("assets/images/clubs.png")),
-                  Expanded(child: Image.asset("assets/images/clubs.png")),
-                ],
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: getWidgets(),
               ),
             ],
           ),
         ),
-        bottomSheet: Container(
-          height: 170,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: Colors.lime[800],
-          ),
-          child: Column(
-            children: [
-              Text(
-                "You won",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 60,
-                  fontWeight: FontWeight.w600,
+        bottomSheet: Visibility(
+          visible: results.length > 0 ? true : false,
+          child: Container(
+            height: 170,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.lime[800],
+            ),
+            child: Column(
+              children: [
+                Text(
+                  checkWin(),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 60,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-                width: double.infinity,
-              ),
-              Container(
-                height: 60,
-                width: 200,
-                decoration: BoxDecoration(
-                  color: Colors.teal[700],
-                  borderRadius: BorderRadius.circular(20),
+                SizedBox(
+                  height: 10,
+                  width: double.infinity,
                 ),
-                child: Center(
-                  child: Text(
-                    "Restart?",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 32,
-                      fontWeight: FontWeight.w400,
+                Container(
+                  width: 200,
+                  height: 65,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        selectedDice = null;
+                        results = [];
+                      });
+                    },
+                    style: ButtonStyle(
+                      // MaterialStateProperty<double?>? elevation,
+                      elevation: MaterialStateProperty.all<double>(20),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.teal),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Restart!!",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold),
+                      ),
                     ),
                   ),
                 ),
-              )
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class BookSum extends StatelessWidget {
-//   const BookSum({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           backgroundColor: Colors.teal,
-//           title: Text("Atomic Habit Summary"),
-//           titleTextStyle: TextStyle(fontStyle: FontStyle.italic),
-//         ),
-//         backgroundColor: Colors.blueGrey,
-//         body: SafeArea(
-//           child: Padding(
-//             padding: const EdgeInsets.only(left: 12.0, right: 8.0, top: 22),
-//             child: Container(
-//               width: 500,
-//               height: 500,
-//               child: Text(
-//                 "The 2nd Law of Behavior change is make it attractive. The more attractive an opportunity is, the more likely it is to become habit forming. Habits are dopamine driven feedback loop. When dopamine rises, so does our motivation to act. It is the anticipation of rewards not the fulfillment of it that gets us to take action. The greater the anticipation, the greater the dopamine spike. Temptation bundling works by linking an action you want to do with an action you need to do. Temptation bundling is one way to apply a psychology theory known as Premack’s principle which states that “more probably behaviors will reinforce less probable behaviors”. ",
-//                 style: TextStyle(
-//                   fontSize: 16.0,
-//                   decoration: TextDecoration.underline,
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
